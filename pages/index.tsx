0f9b501321ee6109635bd340af66a5727357a177
Josué Cortina <joshuacs.main@gmail.com>
2022-08-26 23:49:37 -0400

    build: Migrate all styles to Tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { NextPage } from 'next'
 import Image from 'next/image'
 
-import styles from '../styles/Home.module.css'
 import twitterImg from '../public/twitter-logo.svg'
 
 const TWITTER_HANDLE = '_buildspace';
@@ -9,22 +8,19 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home: NextPage = () => {
   return (
-    <div className={styles['app']}>
-      <div className={styles['container']}>
-        <div className={styles['header-container']}>
-          <p className={styles["header"]}>🖼 GIF Portal</p>
-          <p className={styles["sub-text"]}>
+    <div className='h-screen bg-gray-800 text-center'>
+      <div className='h-full flex relative flex-col justify-center px-8'>
+        <div className='flex flex-col p-8'>
+          <p className='text-5xl font-bold text-white'>🖼 GIF Portal</p>
+          <p className='my-8 text-2xl text-white'>
             View your GIF collection in the metaverse ✨
           </p>
         </div>
-        <div className={styles["footer-container"]}>
+        <div className='flex justify-center items-center absolute w-full bottom-0 left-0 pb-16'>
           <Image alt="Twitter Logo" width={35} height={35} src={twitterImg} />
-          <a
-            className={styles["footer-text"]}
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          <a className='text-white font-bold' href={TWITTER_LINK} target="_blank" rel="noreferrer">
+            {`built on @${TWITTER_HANDLE}`}
+          </a>
         </div>
       </div>
     </div>
