b7d6f4ceeeed9e172de41a80be21ec50eb593fc6
Josué Cortina <joshuacs.main@gmail.com>
2022-08-27 00:37:00 -0400

    build: Install and setup Prettier

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,26 +4,35 @@ const Document = () => {
   return (
     <Html>
       <Head>
-        <meta charSet="utf-8"/>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#000000"/>
-        <meta name="description" content="Web site created using create-react-app" />
-        <meta name="title" content="GIF Portal" />
-        <meta name="description" content="Create a GIF collection with the Metaverse!" />
+        <meta charSet='utf-8' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='theme-color' content='#000000' />
+        <meta name='description' content='Web site created using create-react-app' />
+        <meta name='title' content='GIF Portal' />
+        <meta name='description' content='Create a GIF collection with the Metaverse!' />
 
-        { /* Facebook */ }
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://buildspace.so/" />
-        <meta property="og:title" content="GIF Portal" />
-        <meta property="og:description" content="Create a GIF collection with the Metaverse!" />
-        <meta property="og:image" content="https://s3.amazonaws.com/cdn.buildspace.so/courses/web3-solana-app/metadata.png" />
+        {/* Facebook */}
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://buildspace.so/' />
+        <meta property='og:title' content='GIF Portal' />
+        <meta property='og:description' content='Create a GIF collection with the Metaverse!' />
+        <meta
+          property='og:image'
+          content='https://s3.amazonaws.com/cdn.buildspace.so/courses/web3-solana-app/metadata.png'
+        />
 
-        { /* Twitter */ }
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://buildspace.so/" />
-        <meta property="twitter:title" content="GIF Portal" />
-        <meta property="twitter:description" content="Create a GIF collection with the Metaverse!" />
-        <meta property="twitter:image" content="https://s3.amazonaws.com/cdn.buildspace.so/courses/web3-solana-app/metadata.png" />
+        {/* Twitter */}
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://buildspace.so/' />
+        <meta property='twitter:title' content='GIF Portal' />
+        <meta
+          property='twitter:description'
+          content='Create a GIF collection with the Metaverse!'
+        />
+        <meta
+          property='twitter:image'
+          content='https://s3.amazonaws.com/cdn.buildspace.so/courses/web3-solana-app/metadata.png'
+        />
       </Head>
       <body>
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,20 @@ import Image from 'next/image'
 
 import twitterImg from '../public/twitter-logo.svg'
 
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE = '_buildspace'
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const Home: NextPage = () => {
   return (
     <div className='h-screen bg-gray-800 text-center'>
-      <div className='h-full flex relative flex-col justify-center px-8'>
+      <div className='relative flex h-full flex-col justify-center px-8'>
         <div className='flex flex-col p-8'>
           <p className='text-5xl font-bold text-white'>🖼 GIF Portal</p>
-          <p className='my-8 text-2xl text-white'>
-            View your GIF collection in the metaverse ✨
-          </p>
+          <p className='my-8 text-2xl text-white'>View your GIF collection in the metaverse ✨</p>
         </div>
-        <div className='flex justify-center items-center absolute w-full bottom-0 left-0 pb-16'>
-          <Image alt="Twitter Logo" width={35} height={35} src={twitterImg} />
-          <a className='text-white font-bold' href={TWITTER_LINK} target="_blank" rel="noreferrer">
+        <div className='absolute bottom-0 left-0 flex w-full items-center justify-center pb-16'>
+          <Image alt='Twitter Logo' width={35} height={35} src={twitterImg} />
+          <a className='font-bold text-white' href={TWITTER_LINK} target='_blank' rel='noreferrer'>
             {`built on @${TWITTER_HANDLE}`}
           </a>
         </div>
